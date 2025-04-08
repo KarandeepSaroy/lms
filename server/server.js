12cb8ca928398a9d9efcb55e9e9ead0bd70a3d38
KarandeepSaroy <karandeepsaroy@gmail.com>
2025-04-08 06:41:33 +0530

    resetting the server

--- server/server.js
+++ server/server.js
@@ -1,24 +1,21 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import { connect } from 'mongoose'
 import connectDB from './configs/mongodb.js'
 
-// Initialize express
+// initialize Express
 const app = express()
 
-// Connect to databse
+// Connect to Database
 await connectDB()
 
-// Middleware
+// Middlewares
 app.use(cors())
 
 // Routes
-app.get('/', (req, res) => res.send("API Working"))
-app.post('/clerk', express.json(), clerkWebhooks.js)
+app.get('/', (req, res)=> res.send("API Working"))
 
 // Port
-
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
