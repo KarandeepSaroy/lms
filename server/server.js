536d7f481bc104c773b07a8ad4273f532bb1e055
KarandeepSaroy <karandeepsaroy@gmail.com>
2025-04-15 13:54:40 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
+import { webhooks } from 'svix'
 
 // initialize Express
 const app = express()
@@ -14,6 +15,7 @@ app.use(cors())
 
 // Routes
 app.get('/', (req, res)=> res.send("API Working"))
+app.post('/clerk', express.json(), webhooks )
 
 // Port
 const PORT = process.env.PORT || 5000
